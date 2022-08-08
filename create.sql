903c32483831550858993d0a8dac5f43e3304970
Hadeer Hamdy <92976289+Hadeer-Hamdy@users.noreply.github.com>
2022-08-08 06:52:27 +0200

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,1 @@
+create table international_dept (country_name text , country_code text , indicator_name text ,indicator_code text , dept float );
---
+++ import data.sql
@@ -0,0 +1,1 @@
+copy international_dept from 'D:\international_debt (1).csv' delimiter ',' csv header null 'na';
